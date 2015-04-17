0457d970634f4e4ec1f35fed04f29a05ddbfa08f
Charles+Harris <charles+harris@appacademy.io>
2015-04-17 15:27:48 -0400

    Finish post controller.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,16 +1,42 @@
 class PostsController < ApplicationController
   def new
+    @post = Post.new
   end
 
   def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to post_url(@post)
+    else
+      flash.now[:errors] = @post.errors.full_messages
+      render :new
+    end
   end
 
   def show
+    @post = Post.find(params[:id])
   end
 
   def edit
+    @post = Post.find(params[:id])
   end
 
   def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to post_url(@post)
+    else
+      flash.now[:errors] = @post.errors.full_messages
+      render :new
+    end
   end
+
+  private
+    def post_params
+      params.require(:post).permit(:user_id,
+                                   :content,
+                                   :sub_id,
+                                   :title,
+                                   :url_attrib)
+    end
 end
