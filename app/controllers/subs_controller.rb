5d69f4b7f6c902d444f047c7bbda113bbb263691
Charles+Harris <charles+harris@appacademy.io>
2015-04-17 15:05:14 -0400

    create all routes for subs except for destroy, all routes for posts except index and destroy, write new, create, and sub_params for subs_controller

--- app/controllers/subs_controller.rb
+++ app/controllers/subs_controller.rb
@@ -1,2 +1,36 @@
 class SubsController < ApplicationController
+  def index
+    @subs = Sub.all
+  end
+
+  def new
+    @sub = Sub.new
+  end
+
+  def create
+    @sub = Sub.new(sub_params)
+    if new_sub.save
+      redirect_to sub_url(@sub)
+    else
+      flash.now[:errors] = @sub.errors.full_messages
+      render :new
+    end
+  end
+
+  def show
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  private
+    def sub_params
+        params.require(:sub).permit(:user_id, :title, :description)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   resources :users, except: [:index]
   resources :sessions, only: [ :new, :create, :destroy ]
+  resources :subs, except: [:destroy]
+  resources :posts, except: [:index, :destroy]
 end
