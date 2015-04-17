818914d2d55978d55b5cc2cae28ee9052adf8349
Charles+Harris <charles+harris@appacademy.io>
2015-04-17 14:18:15 -0400

    Finish authentication and credentials.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,7 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by_credentials(user_params)
+    user = User.find_by_credentials(*user_params.values)
     if user
       login!(user)
       redirect_to user_url(user)
@@ -11,11 +11,12 @@ class SessionsController < ApplicationController
       flash.now[:errors] = user.errors.full_messages
       render :new
     end
-    
+
   end
 
   def destroy
-
+    logout!
+    redirect_to new_session_url
   end
 
 end
